682a28b24387279dae93e3f9253217bcc72dfa10
TBINU5SN <tobias.bindewald.external@airbus.com>
2025-04-19 07:18:58 +0000

    Add code of day6

---
+++ day6/escaping_the_maze.py
@@ -0,0 +1,13 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    while right_is_clear():
+        turn_right()
+        move()
+    while wall_on_right() and wall_in_front():
+        turn_left()
+    while wall_on_right() and front_is_clear():
+        move()
