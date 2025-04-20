83d75fe5256b128bca03ba5a1d3ec71174e0811a
TBINU5SN <tobias.bindewald.external@airbus.com>
2025-04-20 07:17:59 +0000

    Add code of day7

--- day7/hangman.py
+++ day7/hangman.py
@@ -26,16 +26,22 @@ print(solution_list)
 
 # Game runs until complete word is guessed
 
+
 while solution_list != word_list:
     guess = input("Guess a letter:\n").lower()
-
+    counter = 0
 # If correct letter is guessed, swap correct placeholders with guessed letters
     for letter in word_list:
-        location = word_list.index(letter)
-        print(location)
+        print(counter)
+        print(letter)
+        if guess == word_list[counter]:
+            solution_list[counter] = guess
+        counter += 1
+    print(solution_list)
+    
+solution = ''.join(solution_list)
 
-
-#solution = ''.join(solution_list)
+print(solution)
 
 
 
