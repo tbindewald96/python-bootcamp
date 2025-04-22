2e58c03c96006665678b34fbb6e478dbce9d5621
tbindewald96 <tobias.bindewald96@gmx.de>
2025-04-22 07:07:30 +0000

    Add code of day8

--- day8/greet.py
+++ day8/greet.py
@@ -1,6 +1,11 @@
-def greet():
-    print("Hello")
-    print("my")
-    print("friend")
+# def greet():
+#     print("Hello")
+#     print("my")
+#     print("friend")
 
-greet()
+# greet()
+
+def greet_with_name(name, location):
+    print(f"Hello {name} from {location}.")
+
+greet_with_name(name = "Tobi", location = "Augsburg")
