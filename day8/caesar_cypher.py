387ae752f8919f9ba752e24bc6637c021c06ad0f
tbindewald96 <tobias.bindewald.external@airbus.com>
2025-04-24 15:24:53 +0000

    add code of day8

--- day8/caesar_cypher.py
+++ day8/caesar_cypher.py
@@ -9,12 +9,10 @@ def caesar(encode_or_decode, original_text, shift_amount):
     for letter in original_text:
         if encode_or_decode == "encode":
             location = alphabet.index(letter) + shift_amount
-            result = "encoded"
         elif encode_or_decode == "decode":
             location = alphabet.index(letter) - shift_amount
-            result = "decoded"
         location %= len(alphabet) 
         text += alphabet[location]
-    print(f"Here is the {result} result: {text}")
+    print(f"Here is the {encode_or_decode} result: {text}")
 
 caesar(encode_or_decode, text, shift)
