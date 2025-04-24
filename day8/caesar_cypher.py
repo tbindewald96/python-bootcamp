4ae4925c5ebdc42e999259ddaa4eb9ad91fd0665
tbindewald96 <tobias.bindewald.external@airbus.com>
2025-04-24 15:23:47 +0000

    add code of day8

--- day8/caesar_cypher.py
+++ day8/caesar_cypher.py
@@ -1,36 +1,20 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+encode_or_decode = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(original_text, shift_amount):
-    encrypted_text = ""
+def caesar(encode_or_decode, original_text, shift_amount):
+    text = ""
     for letter in original_text:
-        location = alphabet.index(letter) + shift
-        if location > len(alphabet)-1:
-            location %= len(alphabet) 
-        encrypted_text += alphabet[location]
-    print(f"Here is the encoded result: {encrypted_text}")
+        if encode_or_decode == "encode":
+            location = alphabet.index(letter) + shift_amount
+            result = "encoded"
+        elif encode_or_decode == "decode":
+            location = alphabet.index(letter) - shift_amount
+            result = "decoded"
+        location %= len(alphabet) 
+        text += alphabet[location]
+    print(f"Here is the {result} result: {text}")
 
-def decrypt(original_text, shift_amount):
-    decrypted_text = ""
-    for letter in original_text:
-        location = alphabet.index(letter) - shift_amount
-        if 0 > location and location > -len(alphabet):
-            location = len(alphabet) + location
-        decrypted_text += alphabet[location]
-        if -len(alphabet) > location:
-            location %= len(alphabet)
-
-
-    print(f"Here is the decoded result: {decrypted_text}")
-
-
-# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
-#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
-
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text, shift)
+caesar(encode_or_decode, text, shift)
