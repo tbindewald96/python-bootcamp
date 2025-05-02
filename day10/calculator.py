9ff0e2fbc7133c5ea3778d6f1c5e66e36b7b9250
TBINU5SN <tobias.bindewald.external@airbus.com>
2025-05-02 13:24:26 +0000

    Add code of day10

--- day10/calculator.py
+++ day10/calculator.py
@@ -2,8 +2,6 @@ from art import logo
 
 print(logo)
 
-input("Whats the first number?")
-
 def add(n1, n2):
     return n1 + n2
 
@@ -15,3 +13,31 @@ def multiply(n1, n2):
 
 def divide(n1, n2):
     return n1 / n2
+
+operations = {
+    "+":add,
+    "-":subtract,
+    "*":multiply,
+    "/":divide
+    }
+
+calculation = True
+
+n1 = float(input("Whats the first number?\n"))
+for symbol in operations:
+    print(symbol)
+operator = input("What is the operation?\n")
+n2 = float(input("What is the second number?\n"))
+
+while calculation == True:
+    print(f"The result of {n1} {operator} {n2} is {operations[operator](n1, n2)}")
+    n1 = float(operations[operator](n1, n2))
+    continue_calculation = input("Do you want to continue to calculate with this number? Type yes or no.\n").lower()
+    if continue_calculation == "yes":
+        operator = input("What is the next operation?\n")
+        n2 = float(input("What is the next number?\n"))
+    else:
+        n1 = 0
+        n1 = float(input("Whats the first number?\n"))
+        operator = input("What is the operation?\n")
+        n2 = float(input("What is the second number?\n"))
