103e1672e61b831728764e3ddb2dc3ed379d9ebd
TBINU5SN <tobias.bindewald.external@airbus.com>
2025-05-04 09:36:32 +0000

    Add code of day11

--- day11/blackjack.py
+++ day11/blackjack.py
@@ -1,7 +1,28 @@
 from art import logo
 
+import random
+
 print(logo)
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-input("Do you want to play a game of blackjack? Type 'y' or 'n'\n")
+input("Do you want to play a game of blackjack? Type 'y' or 'n': ")
+
+player_cards = []
+dealer_cards = []
+
+# first round 
+
+for count in (0,1):
+    player_cards.append(random.choice(cards))
+
+dealer_cards.append(random.choice(cards))
+
+
+print(f"Your cards: {player_cards}, current score: {sum(player_cards)}")
+print(f"Computer's first card: {dealer_cards}")
+
+input("Type 'y' to get another card, type 'n' to pass: ")
+
+player_cards.append(random.choice(cards))
+
