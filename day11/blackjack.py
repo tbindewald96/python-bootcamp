dc39db55be344d1fb40cfbf27146fdb1d5452d8e
tbindewald96 <tobias.bindewald96@gmx.de>
2025-05-05 08:56:25 +0000

    add code of day11

--- day11/blackjack.py
+++ day11/blackjack.py
@@ -6,23 +6,26 @@ print(logo)
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-input("Do you want to play a game of blackjack? Type 'y' or 'n': ")
-
 player_cards = []
 dealer_cards = []
+blackjack = True
 
-# first round 
+def sum_on_hand(party):
+    return sum(party)
 
-for count in (0,1):
-    player_cards.append(random.choice(cards))
+x = input("Do you want to play a game of blackjack? Type 'y' or 'n': ")
 
-dealer_cards.append(random.choice(cards))
+while blackjack == True:
+    if x == "y":
+        for count in (0,1):
+            player_cards.append(random.choice(cards))
+        dealer_cards.append(random.choice(cards))
+        print(f"Your cards: {player_cards}, current score: {sum_on_hand(player_cards)}")
+        print(f"Computer's first card: {dealer_cards}")
 
 
-print(f"Your cards: {player_cards}, current score: {sum(player_cards)}")
-print(f"Computer's first card: {dealer_cards}")
 
 input("Type 'y' to get another card, type 'n' to pass: ")
 
-player_cards.append(random.choice(cards))
+
 
